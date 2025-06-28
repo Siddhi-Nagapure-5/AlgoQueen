b6273a0b22d989f1cd87a0471e5a398b3732178c
Siddhi-Nagapure <siddhi.50it@gmail.com>
2025-06-28 15:13:52 +0530

    New Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // 📍 src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+
 import MyLinks from "./pages/MyLinks";
 function App() {
   return (
